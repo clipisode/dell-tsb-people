84e20f84ad1f1007c721aac6ad54b408a1dd4f8d
Brian Alvey <github@subs.frankensite.com>
2021-10-21 09:50:16 -0700

    remove final frame hack

--- src/getElements/layers/background.ts
+++ src/getElements/layers/background.ts
@@ -58,14 +58,5 @@ export function background(
     position += clip.duration;
   }
 
-  elements.push({
-    type: "frame",
-    name: "endingFrame",
-    // Starts .5 seconds early to account for possible "dark frames" at the end of the last clip
-    startAt: meta.titleDuration + durationOfAllClips - 0.5,
-    endAt: meta.titleDuration + durationOfAllClips + meta.endingDuration,
-    props: { videoKey: endingClip.id, position: "last", ...cover },
-  });
-
   return elements;
 }
